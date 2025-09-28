4bd3ac790614f02c25ce65b22e947709fc921c2a
Zeldaboy14 <zeldaboy1999@gmail.com>
2025-09-28 18:58:26 -0500

    Update content_mounter
    I updated this since Half-Life 2 has changed since the 20th anniversary, and removed 2 redundent entries. Also added CS:GO, pointing towards the cs2 directory for this. L4D, L4D2, and Portal 2 was also added to the mounter. This needs to be optimiazed, as if you load too much, and attempt to skip the games intro vid, it crashs because it doesn't have time to load everything in. Vvis mentioned only loading content in that we need at a time.

--- src/game/shared/content_mounter.cpp
+++ src/game/shared/content_mounter.cpp
@@ -15,57 +15,35 @@
 #include "icommandline.h"
 #include "tier3/tier3.h"
 
-void AddEP2(const char* path)
+void AddHL1(const char* path)
 {
-	filesystem->AddSearchPath(CFmtStr("%s/ep2/ep2_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/ep2/ep2_sound_vo_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/ep2", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/ep2_%language%.txt");
-}
-
-void AddCSS(const char* path)
-{
-	filesystem->AddSearchPath(CFmtStr("%s/cstrike/cstrike_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/cstrike/cstrike_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/cstrike", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/cstrike_%language%.txt");
-}
-
-void AddLostCoast(const char* path)
-{
-	filesystem->AddSearchPath(CFmtStr("%s/lostcoast/lostcoast_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/lostcoast/lostcoast_sound_vo_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/lostcoast", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/lostcoast_%language%.txt");
-}
-
-void AddPortal(const char* path)
-{
-	filesystem->AddSearchPath(CFmtStr("%s/portal/portal_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/portal/portal_sound_vo_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/portal", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/portal_%language%.txt");
-}
-
-void AddEP1(const char* path)
-{
-	filesystem->AddSearchPath(CFmtStr("%s/episodic/ep1_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/episodic/ep1_sound_vo_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/episodic", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/episodic_%language%.txt");
+	filesystem->AddSearchPath(CFmtStr("%s/hl1/hl1_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl1", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/hl1_%language%.txt");
 }
 
 void AddHL2(const char* path)
 {
-	// Crashes for some reason...
-	// We will keep this always in gameinfo for now...
-	/*filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_misc.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_sound_misc.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_textures.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_pak.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_sound_vo_english.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/hl2", path), "GAME", PATH_ADD_TO_HEAD);*/
+	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_misc_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_sound_misc_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_textures_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2/hl2_sound_vo_english_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2_complete/hl2_complete_misc_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/hl2_complete", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/episodic/ep1_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/episodic", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/ep2/ep2_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/ep2", path), "GAME", PATH_ADD_TO_HEAD);
 	g_pVGuiLocalize->AddFile("resource/hl2_%language%.txt");
+}
+
+void AddCSS(const char* path)
+{
+	filesystem->AddSearchPath(CFmtStr("%s/cstrike/cstrike_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/cstrike", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/cstrike_%language%.txt");
 }
 
 void AddHL2MP(const char* path)
@@ -76,80 +54,98 @@ void AddHL2MP(const char* path)
 	g_pVGuiLocalize->AddFile("resource/hl2mp_%language%.txt");
 }
 
-void AddL4D(const char* path)
+void AddLostCoast(const char* path)
 {
-	filesystem->AddSearchPath(CFmtStr("%s/l4d/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/l4d", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/left4dead_%language%.txt");
-}
-
-void AddL4D2(const char* path)
-{
-	filesystem->AddSearchPath(CFmtStr("%s/l4d2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/l4d2", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc1/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc1", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc2", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc3/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc3", path), "GAME", PATH_ADD_TO_HEAD);
-	g_pVGuiLocalize->AddFile("resource/left4dead_%language%.txt");
+	filesystem->AddSearchPath(CFmtStr("%s/lostcoast/lostcoast_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/lostcoast", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/lostcoast_%language%.txt");
 }
 
 void AddTF2(const char* path)
 {
 	filesystem->AddSearchPath(CFmtStr("%s/tf/tf2_misc_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
 	filesystem->AddSearchPath(CFmtStr("%s/tf/tf2_sound_misc_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/tf/tf2_sound_vo_%language%_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/tf/tf2_sound_vo_english_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
 	filesystem->AddSearchPath(CFmtStr("%s/tf/tf2_textures_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
 	filesystem->AddSearchPath(CFmtStr("%s/tf", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/tf_%language%.txt");
 }
 
-void AddPlatform(const char* path)
+void AddPortal(const char* path)
 {
-	filesystem->AddSearchPath(CFmtStr("%s/platform", path), "PLATFORM", PATH_ADD_TO_HEAD);
-	filesystem->AddSearchPath(CFmtStr("%s/platform/platform_misc.vpk", path), "PLATFORM", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal/portal_pak_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/portal_%language%.txt");
+}
+
+void AddL4D(const char* path)
+{
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead_dlc3/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/left4dead_%language%.txt");
+}
+
+void AddL4D2(const char* path)
+{
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc1/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc1", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc2", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc3/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/left4dead2_dlc3", path), "GAME", PATH_ADD_TO_HEAD);
+}
+
+void AddPortal2(const char* path)
+{
+	filesystem->AddSearchPath(CFmtStr("%s/portal2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal2", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal2_dlc1/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal2_dlc1", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal2_dlc2/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/portal2_dlc2", path), "GAME", PATH_ADD_TO_HEAD);
+	g_pVGuiLocalize->AddFile("resource/portal2_%language%.txt");
+}
+
+void AddCSGO(const char* path)
+{
+	filesystem->AddSearchPath(CFmtStr("%s/game/csgo/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/game/csgo", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/game/csgo_core/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/game/csgo_imported/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/game/csgo_lv/pak01_dir.vpk", path), "GAME", PATH_ADD_TO_HEAD);
+	filesystem->AddSearchPath(CFmtStr("%s/csgo", path), "GAME", PATH_ADD_TO_HEAD);
 }
 
 void MountExtraContent()
 {
-	KeyValuesAD gameinfo("GameInfo");
-	gameinfo->LoadFromFile(filesystem, "gameinfo.txt");
+	KeyValuesAD mountdepots("MountDepots");
+	mountdepots->LoadFromFile(filesystem, "cfg/mountdepots.txt");
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(243730))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(280) && (mountdepots->GetBool("hl1")))
 	{
-		char sdk2013SPPath[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(243730, sdk2013SPPath, sizeof(sdk2013SPPath));
-
-		if (gameinfo->GetBool("hl2content") || gameinfo->GetBool("ep1content") || gameinfo->GetBool("ep2content"))
-			AddHL2(sdk2013SPPath);
-
-		if (gameinfo->GetBool("ep2content") || gameinfo->GetBool("ep1content"))
-			AddEP1(sdk2013SPPath);
-
-		if (gameinfo->GetBool("ep2content"))
-			AddEP2(sdk2013SPPath);
-
-		if (gameinfo->GetBool("lostcoastcontent"))
-			AddLostCoast(sdk2013SPPath);
+		char hl1Path[MAX_PATH];
+		steamapicontext->SteamApps()->GetAppInstallDir(280, hl1Path, sizeof(hl1Path));
+		AddHL1(hl1Path);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(243750) && gameinfo->GetBool("hl2mpcontent"))
-	{
-		char sdk2013MPPath[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(243750, sdk2013MPPath, sizeof(sdk2013MPPath));
-		AddHL2(sdk2013MPPath);
-		AddHL2MP(sdk2013MPPath);
-	}
-
-	if (steamapicontext->SteamApps()->BIsAppInstalled(220) && (gameinfo->GetBool("hl2content") || gameinfo->GetBool("ep1content") || gameinfo->GetBool("ep2content")))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(220) && (mountdepots->GetBool("hl2")))
 	{
 		char hl2Path[MAX_PATH];
 		steamapicontext->SteamApps()->GetAppInstallDir(220, hl2Path, sizeof(hl2Path));
 		AddHL2(hl2Path);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(320) && gameinfo->GetBool("hl2mpcontent"))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(240) && mountdepots->GetBool("cstrike"))
+	{
+		char cssPath[MAX_PATH];
+		steamapicontext->SteamApps()->GetAppInstallDir(240, cssPath, sizeof(cssPath));
+		AddCSS(cssPath);
+	}
+
+	if (steamapicontext->SteamApps()->BIsAppInstalled(320) && mountdepots->GetBool("hl2mp"))
 	{
 		char hl2mpPath[MAX_PATH];
 		steamapicontext->SteamApps()->GetAppInstallDir(320, hl2mpPath, sizeof(hl2mpPath));
@@ -157,73 +153,53 @@ void MountExtraContent()
 		AddHL2MP(hl2mpPath);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(380) && (gameinfo->GetBool("ep1content") || gameinfo->GetBool("ep2content")))
-	{
-		char ep1Path[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(380, ep1Path, sizeof(ep1Path));
-		AddEP1(ep1Path);
-	}
-
-	if (steamapicontext->SteamApps()->BIsAppInstalled(420) && gameinfo->GetBool("ep2content"))
-	{
-		char ep2Path[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(420, ep2Path, sizeof(ep2Path));
-		AddEP2(ep2Path);
-	}
-
-	if (steamapicontext->SteamApps()->BIsAppInstalled(340) && gameinfo->GetBool("lostcoastcontent"))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(340) && mountdepots->GetBool("lostcoast"))
 	{
 		char lostCoastPath[MAX_PATH];
 		steamapicontext->SteamApps()->GetAppInstallDir(340, lostCoastPath, sizeof(lostCoastPath));
 		AddLostCoast(lostCoastPath);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(400) && gameinfo->GetBool("portalcontent"))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(440) && mountdepots->GetBool("tf"))
+	{
+		char tf2Path[MAX_PATH];
+		steamapicontext->SteamApps()->GetAppInstallDir(440, tf2Path, sizeof(tf2Path));
+		AddTF2(tf2Path);
+	}
+
+	if (steamapicontext->SteamApps()->BIsAppInstalled(400) && mountdepots->GetBool("portal"))
 	{
 		char portalPath[MAX_PATH];
 		steamapicontext->SteamApps()->GetAppInstallDir(400, portalPath, sizeof(portalPath));
 		AddPortal(portalPath);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(240) && gameinfo->GetBool("csscontent"))
-	{
-		char cssPath[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(240, cssPath, sizeof(cssPath));
-		AddCSS(cssPath);
-	}
-
-	if (steamapicontext->SteamApps()->BIsAppInstalled(440))
-	{
-		char tf2Path[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(440, tf2Path, sizeof(tf2Path));
-
-		if (gameinfo->GetBool("tf2content"))
-			AddTF2(tf2Path);
-	}
-
-	if (steamapicontext->SteamApps()->BIsAppInstalled(500))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(500) && mountdepots->GetBool("left4dead"))
 	{
 		char l4dPath[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(550, l4dPath, sizeof(l4dPath));
-
-		if (gameinfo->GetBool("l4dcontent"))
-			AddL4D(l4dPath);
+		steamapicontext->SteamApps()->GetAppInstallDir(500, l4dPath, sizeof(l4dPath));
+		AddL4D(l4dPath);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(550))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(550) && mountdepots->GetBool("left4dead2"))
 	{
 		char l4d2Path[MAX_PATH];
 		steamapicontext->SteamApps()->GetAppInstallDir(550, l4d2Path, sizeof(l4d2Path));
-
-		if (gameinfo->GetBool("l4d2content"))
-			AddL4D2(l4d2Path);
+		AddL4D2(l4d2Path);
 	}
 
-	if (steamapicontext->SteamApps()->BIsAppInstalled(243750))
+	if (steamapicontext->SteamApps()->BIsAppInstalled(620) && mountdepots->GetBool("portal2"))
 	{
-		char sdk2013MPPath[MAX_PATH];
-		steamapicontext->SteamApps()->GetAppInstallDir(243750, sdk2013MPPath, sizeof(sdk2013MPPath));
-		AddPlatform(sdk2013MPPath);
+		char portal2Path[MAX_PATH];
+		steamapicontext->SteamApps()->GetAppInstallDir(620, portal2Path, sizeof(portal2Path));
+		AddPortal2(portal2Path);
+	}
+
+	if (steamapicontext->SteamApps()->BIsAppInstalled(730) && mountdepots->GetBool("csgo"))
+	{
+		char csgoPath[MAX_PATH];
+		steamapicontext->SteamApps()->GetAppInstallDir(730, csgoPath, sizeof(csgoPath));
+		AddCSGO(csgoPath);
 	}
 
 	filesystem->AddSearchPath(CFmtStr("%s", CommandLine()->ParmValue("-game")), "GAME", PATH_ADD_TO_HEAD);
